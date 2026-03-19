7b8e8211904f1aa2d4f0a3925f2bd08daab7758f
瓜皮 <2396519727@qq.com>
2026-03-19 22:56:28 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -11,6 +11,6 @@ export const GITHUB_CONFIG = {
 	OWNER: process.env.NEXT_PUBLIC_GITHUB_OWNER || 'gua-pi-6',
 	REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || '2025-blog-public',
 	BRANCH: process.env.NEXT_PUBLIC_GITHUB_BRANCH || 'main',
-	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '3123197',
+	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '3133012',
 	ENCRYPT_KEY: process.env.NEXT_PUBLIC_GITHUB_ENCRYPT_KEY || 'wudishiduomejimo',
 } as const
